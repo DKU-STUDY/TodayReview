1961003c5545ce9294dd9296fcbbd4e19b127272
junilhwang <tjsdlf4261@naver.com>
2021-01-06 11:33:55 +0900

    feat: close 작업 추가

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const axios = require("axios");
 const dayjs = require("dayjs");
 
-axios.post(`https://api.github.com/repos/${process.env.REPO}/issues`, {
-  title: dayjs().format("YYYY MM DD"),
-  body: ''
-}, {
-  headers: {
-    authorization: `Bearer ${process.env.TOKEN}`
-  }
-}).then(({ data }) => console.log(data));
+const githubClient = axios.create({
+  baseURL: `https://api.github.com/repos/${process.env.REPO}`,
+  timeout: 5000,
+  headers: { authorization: `Bearer ${process.env.TOKEN}` }
+});
+
+githubClient.interceptors.response.use(({ data }) => data, error => Promise.reject(error));
+
+function fetchIssues() {
+  return githubClient.get(`/issues`);
+}
+
+function postIssue() {
+  return githubClient.post(`/issues`, {
+    title: dayjs().format("YYYY MM DD"),
+    body: ''
+  });
+}
+
+async function main() {
+  const issues = (await fetchIssues()) || [];
+  await Promise.all(issues.map(({ number }) =>
+    githubClient.patch(`/issues/${number}`, { state: 'closed' })
+  ));
+  return postIssue()
+}
+
+main().then(console.log);
